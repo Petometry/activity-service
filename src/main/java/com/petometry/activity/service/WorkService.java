a8b209a8616bb3a519fcb25d01fea93bfb46d46b
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-04-09 12:17:51 +0200

    Update WorkService.java

--- src/main/java/com/petometry/activity/service/WorkService.java
+++ src/main/java/com/petometry/activity/service/WorkService.java
@@ -3,6 +3,7 @@ package com.petometry.activity.service;
 import com.petometry.activity.repository.model.Activity;
 import com.petometry.activity.rest.model.ActivityDto;
 import com.petometry.activity.rest.model.work.WorkActivity;
+import com.petometry.activity.rest.model.WorkDto;
 import org.springframework.security.oauth2.jwt.Jwt;
 
 public interface WorkService {
