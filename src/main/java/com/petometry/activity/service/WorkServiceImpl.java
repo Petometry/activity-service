c6e53dd6f6248c904cdc49815a778a2571de9adc
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-04-09 11:42:49 +0200

    Update WorkServiceImpl.java

--- src/main/java/com/petometry/activity/service/WorkServiceImpl.java
+++ src/main/java/com/petometry/activity/service/WorkServiceImpl.java
@@ -46,6 +46,7 @@ public class WorkServiceImpl implements WorkService {
     @Override
     public void finishActivity(Jwt jwt, Activity activity) {
         currencyService.getPayedByServer(jwt, activity.getOwnerId(), activity.getReward());
+        activity.setCurrency("geocoin");
     }
 
     private static double calculateReward(Activity activity) {
